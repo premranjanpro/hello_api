ff2242fd9f2197a61d6a6ba939148dec792ce7bc
Prem Ranjan <premranjan.pro@gmail.com>
2026-07-09 01:25:56 +0530

    Configure JwtBearer events to read access_token from query parameters for WebSocket connections

--- Program.cs
+++ Program.cs
@@ -32,6 +32,19 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
         ValidateLifetime = true
     };
+    o.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Query["access_token"];
+            var path = context.HttpContext.Request.Path;
+            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+            {
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
 });
 builder.Services.AddAuthorization();
 
